60bd4103ee280cb4075cc716101e9a78c52b01b9
Totli1 <nikto.81@internet.ru>
2025-06-08 16:14:40 +0300

    Добавление проекта

---
+++ MiniPaint/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace MiniPaint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.btnEraser = new System.Windows.Forms.Button();
+            this.btnEllipse = new System.Windows.Forms.Button();
+            this.btnRectangle = new System.Windows.Forms.Button();
+            this.btnLine = new System.Windows.Forms.Button();
+            this.btnPencil = new System.Windows.Forms.Button();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.lblBrushSize = new System.Windows.Forms.Label();
+            this.btnUndo = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnFill = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 102);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(776, 336);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnFill);
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Controls.Add(this.btnOpen);
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Controls.Add(this.btnUndo);
+            this.panel1.Controls.Add(this.btnColor);
+            this.panel1.Controls.Add(this.btnEraser);
+            this.panel1.Controls.Add(this.btnEllipse);
+            this.panel1.Controls.Add(this.btnRectangle);
+            this.panel1.Controls.Add(this.btnLine);
+            this.panel1.Controls.Add(this.btnPencil);
+            this.panel1.Location = new System.Drawing.Point(12, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 42);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(190, 9);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(32, 23);
+            this.btnColor.TabIndex = 5;
+            this.btnColor.Text = "col";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // btnEraser
+            // 
+            this.btnEraser.Location = new System.Drawing.Point(152, 9);
+            this.btnEraser.Name = "btnEraser";
+            this.btnEraser.Size = new System.Drawing.Size(32, 23);
+            this.btnEraser.TabIndex = 4;
+            this.btnEraser.Text = "era";
+            this.btnEraser.UseVisualStyleBackColor = true;
+            this.btnEraser.Click += new System.EventHandler(this.btnEraser_Click);
+            // 
+            // btnEllipse
+            // 
+            this.btnEllipse.Location = new System.Drawing.Point(114, 9);
+            this.btnEllipse.Name = "btnEllipse";
+            this.btnEllipse.Size = new System.Drawing.Size(32, 23);
+            this.btnEllipse.TabIndex = 3;
+            this.btnEllipse.Text = "eli";
+            this.btnEllipse.UseVisualStyleBackColor = true;
+            this.btnEllipse.Click += new System.EventHandler(this.btnEllipse_Click);
+            // 
+            // btnRectangle
+            // 
+            this.btnRectangle.Location = new System.Drawing.Point(76, 9);
+            this.btnRectangle.Name = "btnRectangle";
+            this.btnRectangle.Size = new System.Drawing.Size(32, 23);
+            this.btnRectangle.TabIndex = 2;
+            this.btnRectangle.Text = "rec";
+            this.btnRectangle.UseVisualStyleBackColor = true;
+            this.btnRectangle.Click += new System.EventHandler(this.btnRectangle_Click);
+            // 
+            // btnLine
+            // 
+            this.btnLine.Location = new System.Drawing.Point(38, 9);
+            this.btnLine.Name = "btnLine";
+            this.btnLine.Size = new System.Drawing.Size(32, 23);
+            this.btnLine.TabIndex = 1;
+            this.btnLine.Text = "lin";
+            this.btnLine.UseVisualStyleBackColor = true;
+            this.btnLine.Click += new System.EventHandler(this.btnLine_Click);
+            // 
+            // btnPencil
+            // 
+            this.btnPencil.Location = new System.Drawing.Point(0, 9);
+            this.btnPencil.Name = "btnPencil";
+            this.btnPencil.Size = new System.Drawing.Size(32, 23);
+            this.btnPencil.TabIndex = 0;
+            this.btnPencil.Text = "pen";
+            this.btnPencil.UseVisualStyleBackColor = true;
+            this.btnPencil.Click += new System.EventHandler(this.btnPencil_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 51);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(190, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // lblBrushSize
+            // 
+            this.lblBrushSize.AutoSize = true;
+            this.lblBrushSize.Location = new System.Drawing.Point(208, 63);
+            this.lblBrushSize.Name = "lblBrushSize";
+            this.lblBrushSize.Size = new System.Drawing.Size(75, 13);
+            this.lblBrushSize.TabIndex = 3;
+            this.lblBrushSize.Text = "Размер:  0 px";
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.Location = new System.Drawing.Point(228, 9);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(32, 23);
+            this.btnUndo.TabIndex = 7;
+            this.btnUndo.Text = "und";
+            this.btnUndo.UseVisualStyleBackColor = true;
+            this.btnUndo.Click += new System.EventHandler(this.btnUndo_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(638, 9);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(32, 23);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "sav";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(676, 9);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(32, 23);
+            this.btnOpen.TabIndex = 9;
+            this.btnOpen.Text = "open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(714, 9);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(32, 23);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnFill
+            // 
+            this.btnFill.Location = new System.Drawing.Point(266, 9);
+            this.btnFill.Name = "btnFill";
+            this.btnFill.Size = new System.Drawing.Size(32, 23);
+            this.btnFill.TabIndex = 11;
+            this.btnFill.Text = "fill";
+            this.btnFill.UseVisualStyleBackColor = true;
+            this.btnFill.Click += new System.EventHandler(this.btnFill_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblBrushSize);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.Button btnEraser;
+        private System.Windows.Forms.Button btnEllipse;
+        private System.Windows.Forms.Button btnRectangle;
+        private System.Windows.Forms.Button btnLine;
+        private System.Windows.Forms.Button btnPencil;
+        private System.Windows.Forms.Label lblBrushSize;
+        private System.Windows.Forms.Button btnUndo;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnFill;
+    }
+}
+
---
+++ MiniPaint/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiniPaint
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap _bitmap;
+        private Graphics _graphics;
+        private bool _isDrawing = false;
+        private Point _lastPoint;
+        private Color _currentColor = Color.Black;
+        private int _brushSize = 3;
+        private Tool _currentTool = Tool.Pencil;
+        private Stack<Bitmap> _history = new Stack<Bitmap>();
+
+        enum Tool { Pencil, Line, Rectangle, Ellipse, Eraser }
+        public Form1()
+        {
+            InitializeComponent();
+            _bitmap = new Bitmap(pictureBox.Width, pictureBox.Height);
+            _graphics = Graphics.FromImage(_bitmap);
+            _graphics.Clear(Color.White);
+            pictureBox.Image = _bitmap;
+        }
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            _isDrawing = true;
+            _lastPoint = e.Location;
+            SaveState();
+        }
+
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!_isDrawing) return;
+        
+            using (var g = Graphics.FromImage(_bitmap))
+            {
+                switch (_currentTool)
+                {
+                    case Tool.Pencil:
+                    case Tool.Eraser:
+                        using (var pen = new Pen(
+                            _currentTool == Tool.Eraser ? Color.White : _currentColor,
+                            _brushSize))
+                        {
+                            g.DrawLine(pen, _lastPoint, e.Location);
+                        }
+                        _lastPoint = e.Location;
+                        break;
+
+                    case Tool.Line:
+                    case Tool.Rectangle: 
+                    case Tool.Ellipse:
+                        g.Clear(Color.White);
+                        g.DrawImage(_bitmap, Point.Empty);
+                        DrawShape(g, _lastPoint, e.Location);
+                        break;
+                }
+            }
+        
+            pictureBox.Refresh();
+        }
+        private void DrawShape(Graphics g, Point start, Point end)
+        {
+            using (var pen = new Pen(_currentColor, _brushSize))
+            {
+                switch (_currentTool)
+                {
+                    case Tool.Line:
+                        g.DrawLine(pen, start, end);
+                        break;
+                    case Tool.Rectangle:
+                        g.DrawRectangle(pen, GetRect(start, end));
+                        break;
+                    case Tool.Ellipse:
+                        g.DrawEllipse(pen, GetRect(start, end));
+                        break;
+                }
+            }
+        }
+
+
+        private Rectangle GetRect(Point p1, Point p2)
+        {
+            return new Rectangle(
+                Math.Min(p1.X, p2.X),
+                Math.Min(p1.Y, p2.Y),
+                Math.Abs(p1.X - p2.X),
+                Math.Abs(p1.Y - p2.Y));
+        }
+
+        private void pictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (!_isDrawing) return;
+            _isDrawing = false;
+
+         
+            if (_currentTool != Tool.Pencil && _currentTool != Tool.Eraser)
+            {
+                DrawShape(_graphics, _lastPoint, e.Location);
+                pictureBox.Refresh();
+            }
+        }
+
+        private void btnPencil_Click(object sender, EventArgs e)
+            => _currentTool = Tool.Pencil;
+
+        private void btnLine_Click(object sender, EventArgs e)
+            => _currentTool = Tool.Line;
+
+        private void btnRectangle_Click(object sender, EventArgs e)
+            => _currentTool = Tool.Rectangle;
+
+        private void btnEllipse_Click(object sender, EventArgs e)
+            => _currentTool = Tool.Ellipse;
+
+        private void btnEraser_Click(object sender, EventArgs e)
+            => _currentTool = Tool.Eraser;
+
+     
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                _currentColor = colorDialog.Color;
+            }
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            _brushSize = trackBar1.Value;
+            lblBrushSize.Text = $"Размер: {_brushSize}px";
+        }
+
+       
+
+        private void SaveState()
+        {
+            _history.Push(new Bitmap(_bitmap));
+        }
+
+        private void btnUndo_Click(object sender, EventArgs e)
+        {
+            if (_history.Count > 0)
+            {
+                _bitmap = _history.Pop();
+                _graphics = Graphics.FromImage(_bitmap);
+                pictureBox.Image = _bitmap;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            using (var saveDialog = new SaveFileDialog())
+            {
+                saveDialog.Filter = "PNG Image|*.png|JPEG Image|*.jpg";
+                if (saveDialog.ShowDialog() == DialogResult.OK)
+                {
+                    _bitmap.Save(saveDialog.FileName);
+                }
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            using (var openDialog = new OpenFileDialog())
+            {
+                openDialog.Filter = "Image Files|*.png;*.jpg;*.bmp";
+                if (openDialog.ShowDialog() == DialogResult.OK)
+                {
+                    _bitmap = new Bitmap(openDialog.FileName);
+                    _graphics = Graphics.FromImage(_bitmap);
+                    pictureBox.Image = _bitmap;
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            _graphics.Clear(Color.White);
+            pictureBox.Refresh();
+        }
+
+        private void btnFill_Click(object sender, EventArgs e)
+        {
+            using (var brush = new SolidBrush(_currentColor))
+            {
+                _graphics.FillRectangle(brush, new Rectangle(0, 0, _bitmap.Width, _bitmap.Height));
+                pictureBox.Refresh();
+            }
+        }
+    }
+}
